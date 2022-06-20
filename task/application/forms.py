a70dc5ce4e570828b7c19b9412f7a5ddb62606ed
afzal <kotadia.afzal@gmail.com>
2022-06-20 14:55:03 +0000

    Flask Unit testing covered

--- task/application/forms.py
+++ task/application/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, BooleanField, SelectField, SubmitField
+from wtforms.validators import DataRequired, Length, ValidationError
 
 class TaskForm(FlaskForm):
     task_name = StringField("Task")
